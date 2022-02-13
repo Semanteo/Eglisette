93cfd6c7959f244bd6d1e6363156b51c288264ec
Semanteo <55351251+Semanteo@users.noreply.github.com>
2022-02-13 21:17:40 +0100

    Update update.js

--- .github/workflows/update.js
+++ .github/workflows/update.js
@@ -1,1 +1,10 @@
-console.log("hey")
+const path = "../../articles/vendanges2020.md"
+const fs = require('fs')
+
+fs.readFile(path, 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  console.log(data)
+})
