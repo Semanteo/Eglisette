c1ea244f1875da59b5de0dbfffdd795eb58ed3fe
Semanteo <55351251+Semanteo@users.noreply.github.com>
2022-02-14 09:21:09 +0100

    Update update.js

--- .github/workflows/update.js
+++ .github/workflows/update.js
@@ -18,16 +18,25 @@ fs.readdir(path, function(err, filenames) {
         console.log(content)
         console.log(getTitle(content).text)
         console.log(getDesc(content).text)
-      });
-    });
-  });
-
-
- fs.readFile(path_two, 'utf-8', function(err, content) {
+          
+          fs.readFile(path_two, 'utf-8', function(err, content) {
         if (err) {
           console.log(err);
           return;
         }
         content = JSON.parse(content)
-        console.log(content.lastModified, content["articles"][0].date)
+        console.log(content["articles"])
+        content["articles"].push({
+     "date":1624129211326,
+     "description": getDesc(content).text,
+     "title": getTitle(content).text,
+     "slug": getTitle(content).text.toLowerCase().replace(" ","").replace(" ",""),
+     "image": "https://static.wixstatic.com/media/32b942_93b5b7494fe94c7b873dfe5ea71710cc~mv2_d_2000_3008_s_2.jpg/v1/fill/w_1903,h_543,al_c,q_85,usm_0.66_1.00_0.01/32b942_93b5b7494fe94c7b873dfe5ea71710cc~mv2_d_2000_3008_s_2.webp",
+     "url": `${path + filename} `,
+     "tags": ["vendanges"]
+   })
       });
+      });
+    });
+  });
+
